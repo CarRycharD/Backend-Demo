5af20f064789cc0d542864a7d6bd5fe4ef7bca58
CarRycharD <Richard9871799>
2022-11-05 10:00:57 +0100

    fix(Login.ts): fixilogin problems

--- src/controllers/Login.ts
+++ src/controllers/Login.ts
@@ -10,13 +10,13 @@ const login = (req: Request, res: Response): void => {
     res.status(401).json({
       error: 'invalid login credentials',
     });
+  } else {
+    const token = jwt.sign({ username, password }, SECRET_KEY, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token: token });
   }
-
-  const token = jwt.sign({ username, password }, SECRET_KEY, {
-    expiresIn: '1h',
-  });
-
-  res.status(200).json({ token: token });
 };
 
 export { login };
